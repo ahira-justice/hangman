0633f7449ac7ad1cd7be33df6303924331f077b0
Ahira <justiceahira@gmail.com>
2020-07-17 22:44:42 +0100

    Update game model with fields for max guesses and result

--- core/models.py
+++ core/models.py
@@ -9,6 +9,8 @@ class Game(models.Model):
     difficulty = models.CharField(max_length=1, choices=DIFFICULTY_CHOICES)
     missed_letters = models.CharField(max_length=255, blank=True)
     correct_letters = models.CharField(max_length=255, blank=True)
+    max_guesses = models.IntegerField()
     secret_word = models.CharField(max_length=255)
     secret_set = models.CharField(max_length=255)
     is_done = models.BooleanField(default=False)
+    result = models.CharField(max_length=1, blank=True)
